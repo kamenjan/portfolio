701d8e479e182283588c7cb32b3a249e393f036d
kamenjan <rok.arih@gmail.com>
2019-02-26 22:03:41 +0100

    Updates

---
+++ precache-manifest.d28141aea316dd9460da4a3b49d7750c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "72b1f36e87a263e15162",
+    "url": "/portfolio/static/css/main.1a497912.chunk.css"
+  },
+  {
+    "revision": "72b1f36e87a263e15162",
+    "url": "/portfolio/static/js/main.72b1f36e.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "3f947d08696a904a900b",
+    "url": "/portfolio/static/js/2.3f947d08.chunk.js"
+  },
+  {
+    "revision": "2e76b5fc06a64a8a669c19ea50c3b765",
+    "url": "/portfolio/static/media/meetup.2e76b5fc.jpg"
+  },
+  {
+    "revision": "9022e1f8d1416cba62ec6e2528e85e44",
+    "url": "/portfolio/static/media/avatar.9022e1f8.png"
+  },
+  {
+    "revision": "a56307e4dc59718689c22462525640af",
+    "url": "/portfolio/static/media/white_board.a56307e4.jpg"
+  },
+  {
+    "revision": "7de918fe4f36508c00df6dee80859cd6",
+    "url": "/portfolio/index.html"
+  }
+];
