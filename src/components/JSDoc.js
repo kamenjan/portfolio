b3065937b0adbce27e8f70c05663ca799931369c
kamenjan <rok.arih@gmail.com>
2019-03-11 20:02:27 +0100

    define and use JSDoc component

--- src/components/JSDoc.js
+++ src/components/JSDoc.js
@@ -1,33 +1,148 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledJSDoc = styled.div`
-  overflow: hidden;
-  will-change: rChannel;
-  font-size: 22px;
-  color: grey;
-  p {
-    margin: 0;
-    color: white;
+import { media, size } from '../defines/media'
+
+const Container = styled.div`
+  font-size: 20px;
+  font-family: 'Lucida Console', Monaco, monospace;
+  color: var(--color-contrast, #2b2b2b);
+
+  &::before {
+    color: grey;
+    content: '/**';
+    white-space: pre;
+  }
+
+  &::after {
+    color: grey;
+    content: ' */';
+    white-space: pre;
+  }
+
+  section {
+    position: relative;
+    overflow: hidden;
+    padding-left: 1.8em;
+
+    &::before {
+      color: grey;
+      /* note: "\\A" in "content" create line break */
+      content: '*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A*\\A';
+      white-space: pre;
+      position: absolute;
+      top: 0;
+      left: 0.6em;
+      height: 100%;
+    }
+  }
+
+  @media ${media.MD} {
+    width: 50%;
+  }
+`
+const Tag = styled.span`
+  &:not(:last-child):after {
+    display: block;
+    content: ' ';
+    white-space: pre;
+  }
+
+  ul {
+    li {
+      &:before {
+        content: ' - ';
+        white-space: pre;
+      }
+    }
+  }
+`
+const TagName = styled.span`
+  color: green;
+  &:before {
+    content: '@';
+  }
+  &:after {
+    content: ' ';
   }
 `
 
-const CommentBlock = props => {
+const JSDoc = props => {
   return (
-    <StyledJSDoc>
-      <p>&#47;**</p>
-      {props.children.map((child, i) => (
-        <p key={i}>&nbsp;* {child}</p>
-      ))}
-      <p>&nbsp;*&#47;</p>
-    </StyledJSDoc>
+    <Container>
+      <section>
+        {props.docSections.map(jobSection => {
+          switch (jobSection.type) {
+            case 'paragraph':
+              return (
+                <Tag>
+                  <TagName>{jobSection.docTag}</TagName>
+                  {jobSection.content}
+                </Tag>
+              )
+            case 'array':
+              return (
+                <Tag>
+                  <TagName>{jobSection.docTag}</TagName>
+                  {jobSection.content.map(arrayItem => (
+                    <span>{arrayItem}</span>
+                  ))}
+                </Tag>
+              )
+            case 'list':
+              return (
+                <Tag>
+                  <TagName>{jobSection.docTag}</TagName>
+                  <ul>
+                    {jobSection.content.map(listItem => (
+                      <li>{listItem}</li>
+                    ))}
+                  </ul>
+                </Tag>
+              )
+            default:
+              return null
+          }
+        })}
+      </section>
+    </Container>
   )
 }
 
-const BlockTag = styled.span`
-  color: green;
-`
+JSDoc.defaultProps = {
+  docSections: [
+    {
+      docTag: 'name',
+      type: 'paragraph',
+      content: (
+        <span>
+          <a href=''>termitnjak</a> 2020-2030
+        </span>
+      ),
+    },
+    {
+      docTag: 'summary',
+      type: 'paragraph',
+      content:
+        'Small company offering B2B and B2C web development, consulting and hosting services to EU and worldwide markets.',
+    },
+    {
+      docTag: 'role',
+      type: 'paragraph',
+      content:
+        'I was brought on board as a frontend developer, but later I also assisted on system and cloud administration.',
+    },
+    {
+      docTag: 'responsibilities',
+      type: 'list',
+      content: ['doing this', 'doing that'],
+    },
+    {
+      docTag: 'tags',
+      type: 'array',
+      content: ['javascript', 'html'],
+    },
+  ],
+}
 
-// TODO: Figure out how to memoize named exports vs default exports
-export { CommentBlock, BlockTag }
-// export default React.memo(CommentBlock)
+export default JSDoc
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,17 +4,20 @@ import ThemeContext from '../context/theme'
 
 import { createGlobalStyle } from 'styled-components/macro'
 import 'normalize.css'
+import { media, size } from '../defines/media'
 
 import Header from './Header/Header'
 import About from './About/About'
 import Stack from './Stack/Stack'
-import Work from './Work/Work'
-import Community from './Community/Community'
+
+import JSDoc from './JSDoc'
 import Menu from './Menu/Menu'
 
 import ParallaxDivider from './ParallaxDivider/ParallaxDivider'
 import Desk from './ParallaxDivider/Desk/Desk'
 import City from './ParallaxDivider/City/City'
+
+import styled from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   html { 
@@ -55,7 +58,191 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  ul {
+    list-style-type: none;
+  }
 `
+
+const DocsContainer = styled.div`
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 360px;
+  @media ${media.SM} {
+    max-width: ${`${size.SM}px`};
+  }
+  @media ${media.MD} {
+    max-width: ${`${size.MD}px`};
+  }
+  @media ${media.LG} {
+    max-width: ${`${size.LG}px`};
+  }
+  @media ${media.XL} {
+    max-width: ${`${size.XL}px`};
+  }
+`
+
+const Heading = styled.h1`
+  width: 100%;
+  text-align: center;
+  color: var(--color-tertiary, #2b2b2b);
+  &:before {
+    color: var(--color-primary, #2b2b2b);
+    content: './';
+  }
+  &:after {
+    color: var(--color-primary, #2b2b2b);
+    content: '$';
+  }
+`
+
+const jobs = [
+  {
+    docSections: [
+      {
+        docTag: 'name',
+        type: 'paragraph',
+        content: (
+          <span>
+            <a href=''>termitnjak</a> 2020-2030
+          </span>
+        ),
+      },
+      {
+        docTag: 'summary',
+        type: 'paragraph',
+        content:
+          'Small company offering B2B and B2C web development, consulting and hosting services to EU and worldwide markets.',
+      },
+      {
+        docTag: 'role',
+        type: 'paragraph',
+        content:
+          'I was brought on board as a frontend developer, but later I also assisted on system and cloud administration.',
+      },
+      {
+        docTag: 'responsibilities',
+        type: 'list',
+        content: ['doing this', 'doing that'],
+      },
+      {
+        docTag: 'tags',
+        type: 'array',
+        content: ['javascript', 'html'],
+      },
+    ],
+  },
+  {
+    docSections: [
+      {
+        docTag: 'name',
+        type: 'paragraph',
+        content: (
+          <span>
+            <a href=''>termitnjak</a> 2020-2030
+          </span>
+        ),
+      },
+      {
+        docTag: 'summary',
+        type: 'paragraph',
+        content:
+          'Small company offering B2B and B2C web development, consulting and hosting services to EU and worldwide markets.',
+      },
+      {
+        docTag: 'role',
+        type: 'paragraph',
+        content:
+          'I was brought on board as a frontend developer, but later I also assisted on system and cloud administration.',
+      },
+      {
+        docTag: 'responsibilities',
+        type: 'list',
+        content: ['doing this', 'doing that'],
+      },
+      {
+        docTag: 'tags',
+        type: 'array',
+        content: ['javascript', 'html'],
+      },
+    ],
+  },
+]
+const communityProjects = [
+  {
+    docSections: [
+      {
+        docTag: 'name',
+        type: 'paragraph',
+        content: (
+          <span>
+            <a href=''>termitnjak</a> 2020-2030
+          </span>
+        ),
+      },
+      {
+        docTag: 'summary',
+        type: 'paragraph',
+        content:
+          'Small company offering B2B and B2C web development, consulting and hosting services to EU and worldwide markets.',
+      },
+      {
+        docTag: 'role',
+        type: 'paragraph',
+        content:
+          'I was brought on board as a frontend developer, but later I also assisted on system and cloud administration.',
+      },
+      {
+        docTag: 'responsibilities',
+        type: 'list',
+        content: ['doing this', 'doing that'],
+      },
+      {
+        docTag: 'tags',
+        type: 'array',
+        content: ['javascript', 'html'],
+      },
+    ],
+  },
+  {
+    docSections: [
+      {
+        docTag: 'name',
+        type: 'paragraph',
+        content: (
+          <span>
+            <a href=''>termitnjak</a> 2020-2030
+          </span>
+        ),
+      },
+      {
+        docTag: 'summary',
+        type: 'paragraph',
+        content:
+          'Small company offering B2B and B2C web development, consulting and hosting services to EU and worldwide markets.',
+      },
+      {
+        docTag: 'role',
+        type: 'paragraph',
+        content:
+          'I was brought on board as a frontend developer, but later I also assisted on system and cloud administration.',
+      },
+      {
+        docTag: 'responsibilities',
+        type: 'list',
+        content: ['doing this', 'doing that'],
+      },
+      {
+        docTag: 'tags',
+        type: 'array',
+        content: ['javascript', 'html'],
+      },
+    ],
+  },
+]
 
 const Portfolio = props => {
   const [theme, setTheme] = useState('dark')
@@ -69,8 +256,18 @@ const Portfolio = props => {
       <ParallaxDivider>
         <Desk />
       </ParallaxDivider>
-      <Work />
-      <Community />
+      <DocsContainer>
+        <Heading>Work</Heading>
+        {jobs.map(project => (
+          <JSDoc docSections={project.docSections} />
+        ))}
+      </DocsContainer>
+      <DocsContainer>
+        <Heading>Community</Heading>
+        {communityProjects.map(project => (
+          <JSDoc docSections={project.docSections} />
+        ))}
+      </DocsContainer>
       <ParallaxDivider>
         <City />
       </ParallaxDivider>
