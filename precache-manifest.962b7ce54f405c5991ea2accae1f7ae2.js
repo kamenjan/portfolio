ed230e161b99878498b0e79444fa8859fa7d705e
kamenjan <rok.arih@gmail.com>
2019-03-14 14:56:16 +0100

    Updates

---
+++ precache-manifest.962b7ce54f405c5991ea2accae1f7ae2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "738087a8319155b33a3d",
+    "url": "/portfolio/static/js/main.b1e920db.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4206b0705f1ff1ab10a6",
+    "url": "/portfolio/static/css/2.8b49865a.chunk.css"
+  },
+  {
+    "revision": "4206b0705f1ff1ab10a6",
+    "url": "/portfolio/static/js/2.ea2fe622.chunk.js"
+  },
+  {
+    "revision": "39e2e00b4d1a7478557dc043913f31c6",
+    "url": "/portfolio/static/media/bg-header-xs-dark.39e2e00b.jpg"
+  },
+  {
+    "revision": "99cabc33a27fbc718deaaf836c6c7de3",
+    "url": "/portfolio/static/media/bg-header-sm-dark.99cabc33.jpg"
+  },
+  {
+    "revision": "f876045de835c0be95e81a0c460b50d2",
+    "url": "/portfolio/static/media/bg-header-md-dark.f876045d.jpg"
+  },
+  {
+    "revision": "5a8ae01af33045a8c98c270a2048ae42",
+    "url": "/portfolio/static/media/bg-header-lg-dark.5a8ae01a.jpg"
+  },
+  {
+    "revision": "98c985d434bfe189b65512c634b66735",
+    "url": "/portfolio/static/media/bg-header-xl-dark.98c985d4.jpg"
+  },
+  {
+    "revision": "045deedfc771c66ff36446d346365abd",
+    "url": "/portfolio/static/media/bg-header-xs-light.045deedf.jpg"
+  },
+  {
+    "revision": "ef36fad328a06ded48f3c2ad08ede027",
+    "url": "/portfolio/static/media/bg-header-sm-light.ef36fad3.jpg"
+  },
+  {
+    "revision": "9026bcd5f5ea09ff2e62b3439b152304",
+    "url": "/portfolio/static/media/bg-header-md-light.9026bcd5.jpg"
+  },
+  {
+    "revision": "896961a9f3211b50e2b5f86732ab58e8",
+    "url": "/portfolio/static/media/bg-header-lg-light.896961a9.jpg"
+  },
+  {
+    "revision": "9ffc4451b799a4bcc8ce1c37fde6883f",
+    "url": "/portfolio/static/media/bg-header-xl-light.9ffc4451.jpg"
+  },
+  {
+    "revision": "c171bf463a87fa08e36245b9a9e5d7c6",
+    "url": "/portfolio/static/media/bg-desk-xs-dark.c171bf46.jpg"
+  },
+  {
+    "revision": "9035de9ad57a87f3b625651555eb1bea",
+    "url": "/portfolio/static/media/bg-desk-sm-dark.9035de9a.jpg"
+  },
+  {
+    "revision": "4b561584c7a80704cc1eeac5357892ca",
+    "url": "/portfolio/static/media/bg-desk-md-dark.4b561584.jpg"
+  },
+  {
+    "revision": "8b47441dde3d44783495dad29f4aab21",
+    "url": "/portfolio/static/media/bg-desk-lg-dark.8b47441d.jpg"
+  },
+  {
+    "revision": "98dd4cc15eecdb50849b9432ac028b22",
+    "url": "/portfolio/static/media/bg-desk-xl-dark.98dd4cc1.jpg"
+  },
+  {
+    "revision": "b599902fd20a5eec4dfd745cffe34f8d",
+    "url": "/portfolio/static/media/bg-city-xs-dark.b599902f.jpg"
+  },
+  {
+    "revision": "597f607f03062b3f0284860d22101865",
+    "url": "/portfolio/static/media/bg-city-sm-dark.597f607f.jpg"
+  },
+  {
+    "revision": "a077f8f7dc0beb1d23a29d4d5b4de4a6",
+    "url": "/portfolio/static/media/bg-city-md-dark.a077f8f7.jpg"
+  },
+  {
+    "revision": "dc74ab9f6aff0080d50b39e472348fdd",
+    "url": "/portfolio/static/media/bg-city-lg-dark.dc74ab9f.jpg"
+  },
+  {
+    "revision": "d5a3fdc7096a95e39c940352751ce7d0",
+    "url": "/portfolio/static/media/bg-city-xl-dark.d5a3fdc7.jpg"
+  },
+  {
+    "revision": "2f1669eee18bb1ac92775f89dc545522",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2fdbab89320bc473aecaa8283d155653.js"
+  "/portfolio/precache-manifest.962b7ce54f405c5991ea2accae1f7ae2.js"
 );
 
 workbox.clientsClaim();
