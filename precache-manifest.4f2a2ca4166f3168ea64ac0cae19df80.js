d953f5d78640cf4930442070d74305fd73cd911e
kamenjan <rok.arih@gmail.com>
2020-08-03 22:09:11 +0200

    Updates

---
+++ precache-manifest.4f2a2ca4166f3168ea64ac0cae19df80.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79e3138e1d175daed98c9febbcc0376",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e60dc4a4720b8188fee4",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e60dc4a4720b8188fee4",
+    "url": "/portfolio/static/js/2.1672d020.chunk.js"
+  },
+  {
+    "revision": "398ee93558c4a0f631ffd37914c503e0",
+    "url": "/portfolio/static/js/2.1672d020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94539fe8ee4f51f5e3b",
+    "url": "/portfolio/static/js/main.fd5ab396.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "dc74ab9f6aff0080d50b39e472348fdd",
+    "url": "/portfolio/static/media/bg-city-lg-dark.dc74ab9f.jpg"
+  },
+  {
+    "revision": "a077f8f7dc0beb1d23a29d4d5b4de4a6",
+    "url": "/portfolio/static/media/bg-city-md-dark.a077f8f7.jpg"
+  },
+  {
+    "revision": "597f607f03062b3f0284860d22101865",
+    "url": "/portfolio/static/media/bg-city-sm-dark.597f607f.jpg"
+  },
+  {
+    "revision": "d5a3fdc7096a95e39c940352751ce7d0",
+    "url": "/portfolio/static/media/bg-city-xl-dark.d5a3fdc7.jpg"
+  },
+  {
+    "revision": "b599902fd20a5eec4dfd745cffe34f8d",
+    "url": "/portfolio/static/media/bg-city-xs-dark.b599902f.jpg"
+  },
+  {
+    "revision": "8b47441dde3d44783495dad29f4aab21",
+    "url": "/portfolio/static/media/bg-desk-lg-dark.8b47441d.jpg"
+  },
+  {
+    "revision": "4b561584c7a80704cc1eeac5357892ca",
+    "url": "/portfolio/static/media/bg-desk-md-dark.4b561584.jpg"
+  },
+  {
+    "revision": "9035de9ad57a87f3b625651555eb1bea",
+    "url": "/portfolio/static/media/bg-desk-sm-dark.9035de9a.jpg"
+  },
+  {
+    "revision": "98dd4cc15eecdb50849b9432ac028b22",
+    "url": "/portfolio/static/media/bg-desk-xl-dark.98dd4cc1.jpg"
+  },
+  {
+    "revision": "c171bf463a87fa08e36245b9a9e5d7c6",
+    "url": "/portfolio/static/media/bg-desk-xs-dark.c171bf46.jpg"
+  },
+  {
+    "revision": "5a8ae01af33045a8c98c270a2048ae42",
+    "url": "/portfolio/static/media/bg-header-lg-dark.5a8ae01a.jpg"
+  },
+  {
+    "revision": "f876045de835c0be95e81a0c460b50d2",
+    "url": "/portfolio/static/media/bg-header-md-dark.f876045d.jpg"
+  },
+  {
+    "revision": "99cabc33a27fbc718deaaf836c6c7de3",
+    "url": "/portfolio/static/media/bg-header-sm-dark.99cabc33.jpg"
+  },
+  {
+    "revision": "98c985d434bfe189b65512c634b66735",
+    "url": "/portfolio/static/media/bg-header-xl-dark.98c985d4.jpg"
+  },
+  {
+    "revision": "39e2e00b4d1a7478557dc043913f31c6",
+    "url": "/portfolio/static/media/bg-header-xs-dark.39e2e00b.jpg"
+  }
+]);
